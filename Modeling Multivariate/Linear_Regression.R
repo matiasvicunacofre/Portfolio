423ed57220f2c3fcf0a536f5b2075e531bf74e53
Matias Vicuña Cofré <117483785+matiasvicunacofre@users.noreply.github.com>
2024-01-21 23:19:27 -0300

    Update and rename Ayudantía 2.R to Linear_Regression.R
    changes described in the main of the commit

--- Modeling Multivariate/Linear_Regression.R
+++ Modeling Multivariate/Linear_Regression.R
@@ -1,7 +1,5 @@
-#******** Ayudantía 2 - Regresión Simple ********#
-#********     Profesor: Rodrigo Ortiz     ********#
-#********     Ayudante: Matías Vicuña     ********#
-#******** Mod Regresión Multivariada en R ********#
+#******** Regresión Simple ********#
+#******** Autor: Matías Vicuña ********#
 
 # En caso de no poseer las librerías, correr este código
 install.packages(c("tidyverse", "stargazer", "wooldridge", "lmtest", "car"))
